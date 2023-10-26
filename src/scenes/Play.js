bc6fbb5759529cec9c4b5659e9cfaca6b9d8f350
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2023-10-25 17:25:49 -0700

    Test

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,7 @@ class Play extends Phaser.Scene {
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('fastspaceship', './assets/fastship.png');
         this.load.image('starfield', './assets/starfield.png');
+        
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         this.load.spritesheet('explosion2', './assets/explosion copy.png', {frameWidth: 32, frameHeight: 16, startFrame: 0, endFrame: 9});
